a807914b72586dafabc15346b7bc647b6588eb87
Lily Haverstein <lilyhaverstein@outlook.com>
2022-11-21 18:47:12 -0800

    Continuing to tinker with function.

--- WatchDonna/index.js
+++ WatchDonna/index.js
@@ -2,7 +2,7 @@ import got from "got";
 
 function fetchKnownDates() {
     const knownDateStrings = ["Nov 23,2022","Nov 25,2022","Nov 26,2022","Nov 30,2022","Dec 01,2022","Dec 02,2022","Dec 03,2022","Dec 07,2022","Dec 08,2022","Dec 09,2022","Dec 10,2022","Dec 12,2022","Dec 14,2022","Dec 15,2022","Dec 16,2022","Dec 17,2022","Dec 19,2022","Dec 20,2022","Dec 28,2022","Dec 29,2022","Dec 30,2022","Dec 31,2022"];
-    return new Set(knownDateStrings.map(Date.parse));
+    return new Set(knownDateStrings.map(x => new Date(x)));
 }
 
 function persistKnownDates(context, knownDates) {
@@ -25,11 +25,11 @@ export default async function (context, myTimer) {
     };
 
     const response = await got.post("https://www.vagaro.com/us02/websiteapi/homepage/getavailabledates", { json: requestBody }).json();
-    const responseDates = response.d.map(Date.parse);
+    const responseDates = response.d.map(x => new Date(x));
 
     context.log(`Received ${responseDates.length} dates from Vagaro. Checking for new entries...`);
 
-    newDates = responseDates.filter(x => knownDates.has(x));
+    const newDates = responseDates.filter(x => !knownDates.has(x));
 
     if (newDates.length === 0) {
         context.log('Did not find any new dates of service.');
